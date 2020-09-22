e37e378d2f869dd4fd5d1a700dd74747fea6f49c
Diego Almeida <dyhalmeida@gmail.com>
2020-09-22 07:38:10 -0300

    Rota para criação de projeto e utilização do método POST

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 const app = express();
+const projects = [];
+app.use(express.json());
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+  const project = {
+    id: uuid(),
+    title,
+    owner,
+  };
+  projects.push(project);
+  return response.status(201).json(project);
+});
 app.listen(3333, () => {
   console.log('🚀 Backend started');
 });
