d75d3022bd403fdf87fb5cf8f9a6e4059edd1c9e
Diego Almeida <dyhalmeida@gmail.com>
2020-09-22 07:41:56 -0300

    Adicionado rota de atualização de dados do projeto com método PUT

--- src/index.js
+++ src/index.js
@@ -35,6 +35,19 @@ app.delete('/projects/:id', (request, response) => {
   projects.splice(projectIndex, 1);
   return response.status(204).json();
 });
+
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+  if (projectIndex < 0) {
+    return response.status(400).json(['Project not found']);
+  }
+  const project = { id, title, owner };
+  projects[projectIndex] = project;
+  return response.json(project);
+});
 app.listen(3333, () => {
   console.log('🚀 Backend started');
 });
